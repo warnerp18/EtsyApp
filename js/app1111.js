336803da0aefe4928889f85b4677db256aee9172
warnerp18 <warnerp18@gmail.com>
2014-10-21 10:31:21 -0500

    better

--- js/app1111.js
+++ js/app1111.js
@@ -1,23 +1,21 @@
+
 window.onload = app;
 
 // runs when the DOM is loaded
-
-function app() {
+function app(){
 
     // load some scripts (uses promises :D)
-    loader.load({
-        url: "./bower_components/jquery/dist/jquery.min.js"
-    }, {
-        url: "./bower_components/lodash/dist/lodash.min.js"
-    }, {
-        url: "./bower_components/pathjs/path.min.js"
-    }).then(function() {
+    loader.load(
+        {url: "./bower_components/jquery/dist/jquery.min.js"},
+        {url: "./bower_components/lodash/dist/lodash.min.js"},
+        {url: "./bower_components/jquery-iconify"},
+        {url: "./bower_components/pathjs/path.min.js"}
+    ).then(function(){
         _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
         var options = {
-            api_key: "aavnvygu0h5r52qes74x9zvo"
+            api_key: "e5akcrosbhe7wyu22vdfda2b"
         }
-        // start app?
         var client = new EtsyClient(options);
     })
 
@@ -28,31 +26,33 @@ function EtsyClient(options) {
         throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY!?!?");
     }
     this.etsy_url = "https://openapi.etsy.com/";
-    this.version = options.api_version || "v2/"; // handle api version... if not given, just use the default "v2"
+    this.version = options.api_version || "v2/";
     this.api_key = options.api_key;
     this.complete_api_url = this.etsy_url + this.version;
 
-    // derp.
     this.init();
 }
 
+// this will be getting all the listings (objects) from the Etsy site using our API Key
 EtsyClient.prototype.pullAllActiveListings = function() {
     return $.getJSON(
         this.complete_api_url + "listings/active.js?api_key=" + this.api_key + "&includes=Images&callback=?"
-    )
+        )
     .then(function(data) {
         return data;
     });
 }
 
-EtsyClient.prototype.loadTemplate = function(name){
-    return $.get("./templates/"+name+".html").then(function(){
+// this will load html template files 
+EtsyClient.prototype.loadTemplate = function(name) {
+    return $.get("./templates/" + name + ".html").then(function(){
         return arguments[0];
     })
 }
 
-EtsyClient.prototype.drawListings = function(templateString, data){
-    var grid = document.querySelector("#listings");
+// this will be putting data retrieved above and put it onto the site
+EtsyClient.prototype.drawListings = function(templateString, data) {
+    var grid = document.querySelector("#listings");  
 
     var bigHtmlString = data.results.map(function(listing){
         return _.template(templateString, listing);
@@ -61,11 +61,10 @@ EtsyClient.prototype.drawListings = function(templateString, data){
     grid.innerHTML = bigHtmlString;
 }
 
-EtsyClient.prototype.drawSingleListing = function(id){
+EtsyClient.prototype.drawSingleListing = function(id) {
     var listing = this.latestData.results.filter(function(listing){
         return listing.listing_id === parseInt(id);
     });
-
     var grid = document.querySelector("#listings");
 
     var bigHtmlString = _.template(this.singleListingHtml, listing[0]);
@@ -73,7 +72,7 @@ EtsyClient.prototype.drawSingleListing = function(id){
     grid.innerHTML = bigHtmlString;
 }
 
-EtsyClient.prototype.setupRouting = function(){
+EtsyClient.prototype.setupRouting = function() {
     var self = this;
 
     Path.map("#/").to(function() {
@@ -87,25 +86,33 @@ EtsyClient.prototype.setupRouting = function(){
     Path.map("#/listing/:id").to(function() {
         self.drawSingleListing(this.params.id);
     });
-
-    // set the default hash
+    //set the default hash
     Path.root("#/");
 }
 
-EtsyClient.prototype.init = function(){
+
+EtsyClient.prototype.init = function() {
     var self = this;
     this.setupRouting();
 
     $.when(
         this.pullAllActiveListings(),
-        this.loadTemplate("listing"),
+        this.loadTemplate("Listings"),
         this.loadTemplate("single-page-listing")
-    ).then(function(data, html, singlePageHtml){
+    ).then(function(data, html, singlePageHTML){
 
-        self.latestData = data;
-        self.listingHtml = html;
-        self.singleListingHtml = singlePageHtml;
+        self.latestDate= data;
+        self.listingshtml = html;
+        self.singleListingHtml = singlePageHTML;
 
         Path.listen();
+
     })
 }
+
+
+
+
+
+
+
